e3a7536b768f19383ddcef3d4b823dc576e8d069
prongbang <prongbang@gmail.com>
2019-08-01 13:15:39 +0700

    Initial Commit

---
+++ MoyaEx/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MoyaEx
+//
+//  Created by Endtry on 1/8/2562 BE.
+//  Copyright © 2562 Endtry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
